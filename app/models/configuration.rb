59575368fc2c7ca118d69eb0d41e37920ddba06c
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-14 07:25:09 +0000

    svn:keywords を設定 コメントを追加

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -1,3 +1,11 @@
+#
+# $Id$
+#
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
+# Revision:: $LastChangedRevision$
+# License:: GPL2
+
+
 class Configuration
   @installers_directory = File.expand_path( File.join( RAILS_ROOT, 'installers' ) )
 
@@ -7,3 +15,10 @@ class Configuration
     attr_accessor :installers_directory
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8
+### indent-tabs-mode: nil
+### End:
