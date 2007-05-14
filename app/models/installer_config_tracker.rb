a17d260bd129230a05b0fe1436b1e50afcf1d69c
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-14 08:00:49 +0000

    InstallerConfigTracker のテストを追加 svn:keywords を設定 コメントを追加

--- app/models/installer_config_tracker.rb
+++ app/models/installer_config_tracker.rb
@@ -1,7 +1,15 @@
+#
+# $Id$
+#
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
+# Revision:: $LastChangedRevision$
+# License:: GPL2
+
+
 class InstallerConfigTracker
-  def initialize(installer_path)
-    @central_config_file = File.expand_path(File.join(installer_path, 'work', 'installer.rb'))
-    @local_config_file = File.expand_path(File.join(installer_path, 'installer.rb'))
+  def initialize installer_path
+    @central_config_file = File.expand_path( File.join( installer_path, 'work', 'installer.rb' ) )
+    @local_config_file = File.expand_path( File.join( installer_path, 'installer.rb' ) )
     update_timestamps
   end
 
@@ -11,3 +19,10 @@ class InstallerConfigTracker
     @local_mtime = File.exist?( @local_config_file ) ? File.mtime( @local_config_file ) : nil
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8
+### indent-tabs-mode: nil
+### End:
