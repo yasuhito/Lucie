32e5abdff2dbec23671cfa6445b4f3812afa4e69
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-17 05:30:12 +0000

    Build クラスで使うクラスを追加

---
+++ app/models/config_error.rb
@@ -0,0 +1,17 @@
+#
+# $Id$
+#
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
+# Revision:: $LastChangedRevision$
+# License:: GPL2
+
+
+class ConfigError < StandardError
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8
+### indent-tabs-mode: nil
+### End:
