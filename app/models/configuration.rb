aecd6d3e931c27c32cea3d2d902f248fa660de74
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-17 02:14:45 +0000

    設定可能なプロパティを追加。Changeset [50] のビルドエラーを修正したつもり。

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -7,13 +7,22 @@
 
 
 class Configuration
-  @installers_directory = File.expand_path( File.join( RAILS_ROOT, 'installers' ) )
+  @build_request_checking_interval = 5.seconds
   @dashboard_refresh_interval = 5.seconds
+  @default_polling_interval = 10.seconds
+  @installers_directory = File.expand_path( File.join( RAILS_ROOT, 'installers' ) )
+  @sleep_after_build_loop_error = 30.seconds
+
 
   class << self
+    # published configuration options (mentioned in config/site_config.rb.example)
+    attr_accessor :default_polling_interval
+
     # non-published configuration options.
-    attr_accessor :installers_directory
+    attr_accessor :build_request_checking_interval
     attr_accessor :dashboard_refresh_interval
+    attr_accessor :installers_directory
+    attr_accessor :sleep_after_build_loop_error
   end
 end
 
