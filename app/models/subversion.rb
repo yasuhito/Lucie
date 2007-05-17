5dc60fd43615c1bcff61160f1d07c04b85865fcf
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-17 02:23:16 +0000

    Installer クラスのために、空の Subversion クラスをとりあえず追加

---
+++ app/models/subversion.rb
@@ -0,0 +1,17 @@
+#
+# $Id$
+#
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
+# Revision:: $LastChangedRevision$
+# License:: GPL2
+
+
+class Subversion
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8
+### indent-tabs-mode: nil
+### End:
