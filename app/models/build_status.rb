97589604656e0ad89a36615f0db8058da5da4046
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-17 01:32:41 +0000

    コーディングスタイルについて細かい修正 カバレッジの改善 (100%)

--- app/models/build_status.rb
+++ app/models/build_status.rb
@@ -12,37 +12,8 @@ class BuildStatus
   end
 
 
-  def to_s
-    read_latest_status.to_s
-  end
-
-
-  def status_file
-    return Dir[ "#{ @artifacts_directory }/build_status.*" ].first
-  end
-
-
-  def incomplete?
-    read_latest_status == 'incomplete'
-  end
-
-
-  def match_elapsed_time file_name
-    match =  /^build_status\.[^\.]+\.in(\d+)s$/.match( file_name )
-    if !match or !$1
-      raise 'Could not parse elapsed time.'
-    end
-    return $1.to_i
-  end
-
-
-  def never_built?
-    read_latest_status == 'never_built'
-  end
-
-
   def succeeded?
-    read_latest_status == 'success'
+    return( read_latest_status == 'success' )
   end
 
 
@@ -53,20 +24,64 @@ class BuildStatus
 
 
   def failed?
-    read_latest_status == 'failed'
+    return( read_latest_status == 'failed' )
   end
 
 
-  def fail! elapsed_time, error_message=nil
+  def fail! elapsed_time, error_message = nil
     remove_status_file
     touch_status_file "failed.in#{ elapsed_time }s", error_message
   end
 
+
+  def never_built?
+    return( read_latest_status == 'never_built' )
+  end
+
+
+  def incomplete?
+    return( read_latest_status == 'incomplete' )
+  end
+
+
+  def elapsed_time
+    file = status_file
+    return match_elapsed_time( File.basename( file ) )
+  end
+
+
+  def elapsed_time_in_progress
+    if incomplete? 
+      return ( Time.now - created_at ).ceil
+    end
+    return nil
+  end
+
+
+  def match_elapsed_time file_name
+    match = /\Abuild_status\.[^\.]+\.in(\d+)s\Z/.match( file_name )
+    if !match or !$1
+      raise 'Could not parse elapsed time.'
+    end
+    return $1.to_i
+  end
+
   
+  def to_s
+    return read_latest_status.to_s
+  end
+
+
+  def status_file
+    return Dir[ "#{ @artifacts_directory }/build_status.*" ].first
+  end
+
+
   def created_at
     if file = status_file
-      File.mtime file
+      return File.mtime( file )
     end
+    return nil
   end
 
 
@@ -77,18 +92,10 @@ class BuildStatus
     rescue
       return build_dir_mtime
     end      
-    build_log_mtime > build_dir_mtime ? build_log_mtime : build_dir_mtime
-  end
-
-
-  def elapsed_time
-    file = status_file
-    match_elapsed_time File.basename( file )
-  end
-
-
-  def elapsed_time_in_progress
-    incomplete? ? ( Time.now - created_at ).ceil : nil
+    if build_log_mtime > build_dir_mtime
+      return build_log_mtime
+    end
+    return build_dir_mtime
   end
 
 
@@ -99,8 +106,8 @@ class BuildStatus
     filename = "#{ @artifacts_directory }/build_status.#{ status }"
     FileUtils.touch filename
     if error_message
-      File.open( filename, "w" ) do |f|
-        f.write error_message
+      File.open( filename, 'w' ) do | file |
+        file.write error_message
       end
     end
   end
@@ -113,12 +120,15 @@ class BuildStatus
   
   def read_latest_status
     file = status_file
-    file ? match_status( File.basename( file ) ).downcase : 'never_built'
+    if file
+      return match_status( File.basename( file ) ).downcase
+    end
+    return 'never_built'
   end
 
 
   def match_status file_name
-    /^build_status\.([^\.]+)(\..+)?/.match( file_name )[ 1 ]
+    return /\Abuild_status\.([^\.]+)(\..+)?/.match( file_name )[ 1 ]
   end
 end
 
--- test/unit/build_status_test.rb
+++ test/unit/build_status_test.rb
@@ -30,31 +30,31 @@ class BuildStatusTest < Test::Unit::TestCase
 
 
   def test_never_built_is_true_when_file_is_missing
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [] )
     assert_equal true, BuildStatus.new( 'artifacts_directory' ).never_built?
   end
 
 
   def test_never_built_is_false_when_file_exists
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.anything' ] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.anything' ] )
     assert_equal false, BuildStatus.new( 'artifacts_directory' ).never_built?
   end
 
 
   def test_succeeded_is_true_when_file_is___success__
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.success' ] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.success' ] )
     assert_equal true, BuildStatus.new( 'artifacts_directory' ).succeeded?
   end
 
 
   def test_succeeded_is_false_when_file_is_not___success__
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [] )
     assert_equal false, BuildStatus.new( 'artifacts_directory' ).succeeded?
   end
 
 
   def test_succeed_creates_file___success
-    Dir.stubs( :'[]' ).returns( [ 'artifacts_directory/build_status.foo' ] )
+    Dir.stubs( :[] ).returns( [ 'artifacts_directory/build_status.foo' ] )
     FileUtils.expects( :rm_f ).with( [ 'artifacts_directory/build_status.foo' ] )
     FileUtils.expects( :touch ).with( 'artifacts_directory/build_status.success.in3.5s' )
     BuildStatus.new( 'artifacts_directory' ).succeed!( 3.5 )
@@ -62,34 +62,48 @@ class BuildStatusTest < Test::Unit::TestCase
 
 
   def test_failed_is_true_when_file_is___failed__
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.failed.in3.5s' ] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.failed.in3.5s' ] )
     assert_equal true, BuildStatus.new( 'artifacts_directory' ).failed?
   end
 
 
   def test_failed_is_false_when_file_is_not___failed__
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [] )
     assert_equal false, BuildStatus.new( 'artifacts_directory' ).failed?
   end
 
 
   def test_fail_creates_file___failed__
-    Dir.stubs( :'[]' ).returns( [ 'artifacts_directory/build_status.foo' ] )
+    Dir.stubs( :[] ).returns( [ 'artifacts_directory/build_status.foo' ] )
     FileUtils.expects( :rm_f ).with( [ 'artifacts_directory/build_status.foo' ] )
     FileUtils.expects( :touch ).with( 'artifacts_directory/build_status.failed.in3.5s' )
-    BuildStatus.new( 'artifacts_directory' ).fail!( 3.5 )
+    file_mock = mock()
+    file_mock.expects( :write ).with( 'ERROR_MESSAGE' )
+    File.stubs( :open ).with( 'artifacts_directory/build_status.failed.in3.5s', 'w' ).yields( file_mock )
+    BuildStatus.new( 'artifacts_directory' ).fail!( 3.5, 'ERROR_MESSAGE' )
   end
 
 
   def test_created_at_returns_creation_time_for_status_file
     now = Time.now
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [ :some_file ] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [ :some_file ] )
     File.expects( :mtime ).with( :some_file ).returns( now )
     assert_equal now, BuildStatus.new( 'artifacts_directory' ).created_at
   end
 
 
-  def test_timestamp_returns_later_mtime_of_build_log_or_build_dir
+  def test_timestamp_returns_mtime_of_build_log
+    build_log_mtime = Time.now
+    File.expects( :mtime ).with( 'artifacts_directory/build.log' ).returns( build_log_mtime )
+
+    build_dir_mtime = 2.days.ago
+    File.expects( :mtime ).with( 'artifacts_directory' ).returns( build_dir_mtime )
+
+    assert_equal build_log_mtime, BuildStatus.new( 'artifacts_directory' ).timestamp
+  end
+
+
+  def test_timestamp_returns_mtime_of_build_dir
     build_log_mtime = Time.now
     File.expects( :mtime ).with( 'artifacts_directory/build.log' ).returns( build_log_mtime )
 
@@ -109,7 +123,7 @@ class BuildStatusTest < Test::Unit::TestCase
 
 
   def test_created_at_returns_nil_when_file_not_exist
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [] )
     assert_equal nil, BuildStatus.new( 'artifacts_directory' ).created_at
   end
 
@@ -120,13 +134,13 @@ class BuildStatusTest < Test::Unit::TestCase
 
 
   def test_elapsed_time_should_return_elapsed_seconds_if_build_succeeded
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.success.in3s' ] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.success.in3s' ] )
     assert_equal 3, BuildStatus.new( 'artifacts_directory' ).elapsed_time
   end
 
 
   def test_elapsed_time_should_return_blank_if_elapsed_time_not_availabe
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.hoo' ] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.hoo' ] )
     assert_raises( 'Could not parse elapsed time.' ) do
       BuildStatus.new( 'artifacts_directory' ).elapsed_time
     end
@@ -134,7 +148,7 @@ class BuildStatusTest < Test::Unit::TestCase
 
 
   def test_elapsed_time_in_progress
-    Dir.expects( :'[]' ).at_least( 1 ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.incomplete' ] )
+    Dir.expects( :[] ).at_least( 1 ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.incomplete' ] )
     File.expects( :mtime ).with( 'build_status.incomplete' ).returns( Time.local( 2000, 'jan', 1, 20, 15, 1 ) )
     Time.expects( :now ).returns( Time.local( 2000, 'jan', 1, 20, 15, 10 ) )
     assert_equal 9, BuildStatus.new( 'artifacts_directory' ).elapsed_time_in_progress
@@ -142,15 +156,16 @@ class BuildStatusTest < Test::Unit::TestCase
 
 
   def test_elapsed_time_in_progress_should_return_nil_when_not_incomplete
-    Dir.expects( :'[]' ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.success.in123s' ] )
+    Dir.expects( :[] ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.success.in123s' ] )
     assert_nil BuildStatus.new( 'artifacts_directory' ).elapsed_time_in_progress
   end
 
 
   def test_elapsed_time_in_progress_ceils_fractionals
-    Dir.expects( :'[]' ).at_least( 1 ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.incomplete' ] )
+    Dir.expects( :[] ).at_least( 1 ).with( 'artifacts_directory/build_status.*' ).returns( [ 'build_status.incomplete' ] )
     File.expects( :mtime ).with( 'build_status.incomplete' ).returns( Time.local( 2000, 'jan', 1, 20, 15, 1 ) )
-    time_with_fractional_seconds = Time.local( 2000, 'jan', 1, 20, 15, 10 ) + 0.2 #difference is 9.2 seconds
+    # difference is 9.2 seconds
+    time_with_fractional_seconds = Time.local( 2000, 'jan', 1, 20, 15, 10 ) + 0.2
     Time.expects( :now ).returns( time_with_fractional_seconds )
     assert_equal 10, BuildStatus.new( 'artifacts_directory' ).elapsed_time_in_progress
   end
