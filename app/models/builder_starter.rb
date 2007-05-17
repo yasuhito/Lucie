8147802a21ef21ddbbeba76869323f207b746d77
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-17 02:32:44 +0000

    Installer クラスのために、空の BuilderStarter クラスを追加

---
+++ app/models/builder_starter.rb
@@ -0,0 +1,17 @@
+#
+# $Id$
+#
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
+# Revision:: $LastChangedRevision$
+# License:: GPL2
+
+
+class BuilderStarter
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8
+### indent-tabs-mode: nil
+### End:
---
+++ test/unit/builder_starter_test.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+#
+# $Id$
+#
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
+# Revision:: $LastChangedRevision$
+# License:: GPL2
+
+
+require File.dirname(__FILE__) + '/../test_helper'
+
+
+class BuilderStarterTest
+  def test_truth
+    assert true
+  end
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8
+### indent-tabs-mode: nil
+### End:
