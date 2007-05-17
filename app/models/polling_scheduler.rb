19f5601c3a5037c3f79c7d716c7053ab6aa090d8
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-17 02:11:03 +0000

    テストをどんどん追加

--- app/models/polling_scheduler.rb
+++ app/models/polling_scheduler.rb
@@ -13,6 +13,88 @@ class PollingScheduler
     @last_build_loop_error_source = nil
     @last_build_loop_error_time = nil
   end
+
+
+  def polling_interval= value
+    begin
+      value = value.to_i
+    rescue 
+      raise "Polling interval value #{ value.inspect } could not be converted to a number of seconds"
+    end
+    if value < 5.seconds
+      raise "Polling interval of #{ value } seconds is too small (min. 5 seconds)"
+    end
+    if value > 24.hours
+      raise "Polling interval of #{value} seconds is too big (max. 24 hours)"
+    end
+    @custom_polling_interval = value
+  end
+
+
+  def last_logged_less_than_an_hour_ago
+    @last_build_loop_error_time and @last_build_loop_error_time >= 1.hour.ago
+  end
+
+
+  def run
+    loop do
+     begin
+       @installer.build_if_necessary or check_build_request_until_next_polling
+       clean_last_build_loop_error
+       # TODO looks like throwing isn't necessary anymore, can simply return :reload_installer 
+       if @installer.config_modified?
+         throw :reload_installer
+       end
+     rescue => e
+       # TODO test this code block.
+       unless ( same_error_as_before( e ) and last_logged_less_than_an_hour_ago )
+         log_error e
+       end
+       sleep Configuration.sleep_after_build_loop_error
+     end
+    end
+  end
+
+
+  def check_build_request_until_next_polling
+    time_to_go = Time.now + polling_interval
+    while Time.now < time_to_go
+      @installer.build_if_requested
+      sleep build_request_checking_interval
+    end
+  end
+
+
+  def polling_interval
+    @custom_polling_interval or Configuration.default_polling_interval
+  end
+
+
+  def build_request_checking_interval
+    return Configuration.build_request_checking_interval
+  end
+
+
+  def same_error_as_before(error)
+    @last_build_loop_error_source and (error.backtrace.first == @last_build_loop_error_source)
+  end
+
+
+  def log_error(error)
+    begin
+      CruiseControl::Log.error(error) 
+    rescue 
+      STDERR.puts(error.message)
+      STDERR.puts(error.backtrace.map { |l| "  #{l}"}.join("\n"))
+    end
+    @last_build_loop_error_source = error.backtrace.first
+    @last_build_loop_error_time = Time.now
+  end
+
+
+  def clean_last_build_loop_error
+    @last_build_loop_error_source = @last_build_loop_error_time = nil
+  end
 end
 
 
--- test/unit/polling_scheduler_test.rb
+++ test/unit/polling_scheduler_test.rb
@@ -11,9 +11,78 @@ require File.dirname( __FILE__ ) + '/../test_helper'
 
 
 class PollingSchedulerTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def setup
+    @mock_installer = Object.new
+    @scheduler = PollingScheduler.new( @mock_installer )
+  end
+
+
+  def test_polling_interval_default_value_and_overriding
+    assert_equal Configuration.default_polling_interval, @scheduler.polling_interval
+    @scheduler.polling_interval = 1.minute
+    assert_equal 60, @scheduler.polling_interval
+  end
+
+
+  def test_polling_interval_limits
+    assert_nothing_raised do
+      @scheduler.polling_interval = 5.seconds
+    end
+
+    assert_raises( 'Polling interval of 4 seconds is too small (min. 5 seconds)' ) do
+      @scheduler.polling_interval = 4.seconds
+    end
+
+    assert_nothing_raised do
+      @scheduler.polling_interval = 24.hours
+    end
+
+    assert_raises( 'Polling interval of 86401 seconds is too big (max. 24 hours)' ) do
+      @scheduler.polling_interval = 24.hours + 1.second
+    end
+  end
+
+
+  def test_invalid_polling_interval_raises_exception
+    assert_raises( 'Polling interval value {} could not be converted to a number of seconds' ) do
+      @scheduler.polling_interval = {}
+    end
+  end
+
+
+  def test_last_logged_less_than_an_hour_ago
+    assert !@scheduler.last_logged_less_than_an_hour_ago
+  
+    @scheduler.instance_eval( "@last_build_loop_error_time = DateTime.new( 2005, 1, 1 )" )
+
+    time = DateTime.new( 2005, 1, 1 )
+
+    Time.stubs( :now ).returns( time + 1.hour )
+    assert @scheduler.last_logged_less_than_an_hour_ago
+    
+    Time.stubs( :now ).returns( time + 1.hour + 1.second )
+    assert !@scheduler.last_logged_less_than_an_hour_ago
+  end
+
+
+  def test_check_build_request_until_next_polling
+    @scheduler.expects( :polling_interval ).returns( 2.seconds )
+    @scheduler.stubs( :build_request_checking_interval ).returns( 0 )
+    Time.expects( :now ).times( 4 ).returns( Time.at( 0 ), Time.at( 0 ), Time.at( 1 ), Time.at( 2 ) )
+    @mock_installer.expects( :build_if_requested ).times( 2 )
+
+    @scheduler.check_build_request_until_next_polling
+  end
+
+
+  def test_should_return_flag_to_reload_installer_if_configurations_modified
+    @scheduler.expects( :check_build_request_until_next_polling ).returns( false )
+    @mock_installer.expects( :build_if_necessary ).returns( nil )
+    @mock_installer.expects( :config_modified? ).returns( true ) 
+
+    assert_throws( :reload_installer ) do
+      @scheduler.run
+    end
   end
 end
 
