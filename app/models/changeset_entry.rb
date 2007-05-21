66123b8a58d952827051490c247a24a55e96f16e
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-21 00:19:54 +0000

    コメントを追加

--- app/models/changeset_entry.rb
+++ app/models/changeset_entry.rb
@@ -1,3 +1,11 @@
+#
+# $Id$
+#
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
+# Revision:: $LastChangedRevision$
+# License:: GPL2
+
+
 ChangesetEntry = Struct.new :operation, :file
 
 
@@ -6,3 +14,10 @@ class ChangesetEntry
     return "  #{ operation } #{ file }"
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8
+### indent-tabs-mode: nil
+### End:
