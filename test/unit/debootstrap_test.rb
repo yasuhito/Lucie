a594a22af8bd1e24808bc942adc23463b64ed9a2
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-29 04:47:32 +0000

    リファクタリング

--- test/unit/debootstrap_test.rb
+++ test/unit/debootstrap_test.rb
@@ -9,24 +9,24 @@
 
 require File.dirname( __FILE__ ) + '/../test_helper'
 
-require 'lib/popen3/debootstrap'
+require 'popen3/debootstrap'
 
 
-class TC_Debootstrap < Test::Unit::TestCase
+class DebootstrapTest < Test::Unit::TestCase
   include Debootstrap
 
 
-  def test_version
+  def test_debootstrap_version
     shell_mock = mock( 'SHELL' )
     shell_mock.expects( :on_stdout ).yields( 'ii  debootstrap    0.2.45-0.2     Bootstrap a basic Debian system' )
     shell_mock.expects( :exec ).with( { 'LC_ALL' => 'C' }, 'dpkg', '-l' )
     Popen3::Shell.expects( :open ).yields( shell_mock )
 
-    assert_match( /[\d\.\-]+/, Popen3::Debootstrap.VERSION )
+    assert_equal( '0.2.45-0.2', Popen3::Debootstrap.VERSION )
   end
 
 
-  def test_new
+  def test_debootstrap_new___success___
     shell_mock = mock( 'SHELL', :on_stdout => nil, :on_stderr => nil, :on_failure => nil, :exec => nil, :child_status => 'CHILD_STATUS' )
     Popen3::Shell.expects( :open ).yields( shell_mock ).returns( shell_mock )
 
@@ -43,7 +43,7 @@ class TC_Debootstrap < Test::Unit::TestCase
   end
 
 
-  def test_abbreviation
+  def test_abbreviation___success___
     shell_mock = mock( 'SHELL', :on_stdout => nil, :on_stderr => nil, :on_failure => nil, :exec => nil, :child_status => 'CHILD_STATUS' )
     Popen3::Shell.expects( :open ).yields( shell_mock ).returns( shell_mock )
 
@@ -57,6 +57,72 @@ class TC_Debootstrap < Test::Unit::TestCase
     end
 
     assert_equal 'CHILD_STATUS', debootstrap.child_status
+  end
+
+
+  def test_on_stdout_called
+    shell_mock = mock( 'SHELL', :on_stderr => nil, :on_failure => nil, :exec => nil )
+    shell_mock.expects( :on_stdout ).yields( 'STDOUT' )
+    Lucie::Log.expects( :debug ).with( 'STDOUT' ).times( 1 )
+    Popen3::Shell.expects( :open ).yields( shell_mock ).returns( shell_mock )
+
+    debootstrap = Popen3::Debootstrap.new do | option |
+      option.env = { 'TEST_ENV_NAME' => 'TEST_ENV_VALUE' }
+      option.exclude = [ 'DHCP-CLIENT', 'INFO' ]
+      option.suite = 'WOODY'
+      option.target = '/TMP'
+      option.mirror = 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/'
+      option.include = [ 'INCLUDE' ]
+    end
+  end
+
+
+  def test_on_stderr_called
+    shell_mock = mock( 'SHELL', :on_stdout => nil, :on_failure => nil, :exec => nil )
+    shell_mock.expects( :on_stderr ).yields( 'STDERR' )
+    Lucie::Log.expects( :error ).with( 'STDERR' ).times( 1 )
+    Popen3::Shell.expects( :open ).yields( shell_mock ).returns( shell_mock )
+
+    debootstrap = Popen3::Debootstrap.new do | option |
+      option.env = { 'TEST_ENV_NAME' => 'TEST_ENV_VALUE' }
+      option.exclude = [ 'DHCP-CLIENT', 'INFO' ]
+      option.suite = 'WOODY'
+      option.target = '/TMP'
+      option.mirror = 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/'
+      option.include = [ 'INCLUDE' ]
+    end
+  end
+
+
+  def test_on_stderr_should_raises_runtime_error
+    shell_mock = mock( 'SHELL', :on_stdout => nil )
+    shell_mock.expects( :on_stderr ).yields( 'ln: ??? File exists' )
+    Popen3::Shell.expects( :open ).yields( shell_mock ).returns( shell_mock )
+
+    assert_raises( 'ln: ??? File exists' ) do
+      debootstrap = Popen3::Debootstrap.new do | option |
+      end
+    end
+  end
+
+
+  def test_on_failure_should_raises_runtime_error
+    shell_mock = mock( 'SHELL', :on_stdout => nil )
+    shell_mock.expects( :on_stderr ).yields( 'STDERR' )
+    shell_mock.expects( :on_failure ).yields
+    Lucie::Log.expects( :error ).with( 'STDERR' ).times( 1 )
+    Popen3::Shell.expects( :open ).yields( shell_mock ).returns( shell_mock )
+
+    assert_raises( 'STDERR' ) do
+      debootstrap = Popen3::Debootstrap.new do | option |
+        option.env = { 'TEST_ENV_NAME' => 'TEST_ENV_VALUE' }
+        option.exclude = [ 'DHCP-CLIENT', 'INFO' ]
+        option.suite = 'WOODY'
+        option.target = '/TMP'
+        option.mirror = 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/'
+        option.include = [ 'INCLUDE' ]
+      end
+    end
   end
 end
 
