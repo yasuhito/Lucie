b9d3cb219694a9b77bc416a2736684bd8079c3e8
Yasuhito Takamiya <yasuhito@gmail.com>
2007-05-29 09:07:56 +0000

    インストーラのビルドが成功するテスト

--- config/demo/package.rb
+++ config/demo/package.rb
@@ -1,6 +1,6 @@
 # Package configuration for Lucie
 
-aptget_install 'ssh', 'linuxlogo', 'file', 'less', 'time', 'foo'
+aptget_install 'ssh', 'linuxlogo', 'file', 'less', 'time'
 aptget_remove 'ppp'
 aptget_clean
 
