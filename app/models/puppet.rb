a3d7b1ae5fb1553cc1fd604d2355b598d187c99a
Yasuhito Takamiya <yasuhito@gmail.com>
2007-09-10 07:55:01 +0000

    puppetca --clean を自動的に実行するように修正 リファクタリング

--- app/models/puppet.rb
+++ app/models/puppet.rb
@@ -1,14 +1,35 @@
+require 'facter'
+require 'lib/popen3/shell'
+
+
 class Puppet
   def self.setup local_checkout_dir
-    manifest_dir = File.join( local_checkout_dir, 'puppet/manifests' )
-    files_dir = File.join( local_checkout_dir, 'puppet/files' )
+    self.new.setup local_checkout_dir
+  end
 
+
+  attr_reader :manifest_dir
+  attr_reader :files_dir
+
+
+  def setup local_checkout_dir
+    @manifest_dir = File.join( local_checkout_dir, 'puppet/manifests' )
+    @files_dir = File.join( local_checkout_dir, 'puppet/files' )
+
+    check_puppet_installed
+    write_config
+    restart_puppet
+  end
+
+
+  def check_puppet_installed
     unless File.exists?( '/usr/sbin/puppetmasterd' )
-      puts 'FAILED: puppetmaster package is not installed. Please install first.'
-      puts ' % sudo aptitude install puppetmaster'
-      exit 1
+      raise 'puppetmaster package is not installed. Please install first.'
     end
+  end
 
+
+  def write_config
     File.open( '/etc/puppet/puppetmasterd.conf', 'w' ) do | file |
       file.puts( <<-EOF )
 [puppetmasterd]
@@ -31,7 +52,13 @@ EOF
   allow *
 EOF
     end
+  end
 
-    system '/etc/init.d/puppetmaster restart'
+
+  def restart_puppet
+    Nodes.load_all.list.each do | each |
+      sh_exec "puppetca --clean #{ each.name }.#{ Facter.value( 'domain' ) }"
+    end
+    sh_exec '/etc/init.d/puppetmaster restart'
   end
 end
