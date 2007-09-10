d57c01518ca926d722d504124c2faf6536c16858
Yasuhito Takamiya <yasuhito@gmail.com>
2007-09-10 23:58:08 +0000

    NFS モデルのリファクタリング

--- app/models/nfs.rb
+++ app/models/nfs.rb
@@ -1,18 +1,43 @@
 require 'ftools'
+require 'popen3/shell'
 
 
 class Nfs
   def self.setup installer_name
-    nodes = Nodes.load_enabled( installer_name ).collect do | each |
-      each.name
-    end.join( ',' )
+    self.new.setup installer_name
+  end
 
-    config_file = '/etc/exports'
+
+  attr_reader :installer_name
+
+
+  def setup installer_name
+    @installer_name = installer_name
+
     File.copy config_file, config_file + '.orig'
 
     File.open( config_file, 'w' ) do | file |
-      file.puts File.expand_path( "#{ RAILS_ROOT }/installers/#{ installer_name }/nfsroot #{ nodes }(async,ro,no_root_squash,no_subtree_check)" )
+      file.puts "#{ nfsroot } #{ nodes.join( ',' ) }(async,ro,no_root_squash,no_subtree_check)"
     end
-    system '/etc/init.d/nfs-kernel-server restart'
+
+    sh_exec '/etc/init.d/nfs-kernel-server restart'
+  end
+
+
+  # [???] get nfsroot path from other class (Nfsroot or Installers)?
+  def nfsroot
+    File.expand_path( "#{ RAILS_ROOT }/installers/#{ installer_name }/nfsroot" )
+  end
+
+
+  def nodes
+    Nodes.load_enabled( installer_name ).collect do | each |
+      each.name
+    end
+  end
+
+
+  def config_file
+    return '/etc/exports'
   end
 end
--- test/unit/nfs_test.rb
+++ test/unit/nfs_test.rb
@@ -1,10 +1,24 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname( __FILE__ ) + '/../test_helper'
+
 
 class NfsTest < Test::Unit::TestCase
-  fixtures :nfs
+  def test_setup___SUCCESS___
+    nfs = Nfs.new
+    Nfs.stubs( :new ).returns( nfs )
+    nfs.stubs( :sh_exec )
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+    file = Object.new
+    file.stubs( :puts )
+
+    File.stubs( :copy ).with( '/etc/exports', '/etc/exports.orig' )
+    File.stubs( :open ).with( '/etc/exports', 'w' ).yields( file )
+
+    node = Object.new
+    node.stubs( :name ).returns( 'NODE_NAME' )
+    Nodes.stubs( :load_enabled ).with( 'INSTALLER_NAME' ).returns( [ node ] )
+
+    assert_nothing_raised do
+      Nfs.setup( 'INSTALLER_NAME' )
+    end
   end
 end
