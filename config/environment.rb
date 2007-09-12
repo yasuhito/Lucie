3f6b794f84d6fa90f58151c5248e5726edba5948
Yasuhito Takamiya <yasuhito@gmail.com>
2007-09-12 08:09:51 +0000

    不要なコメントやホワイトスペースを削除

--- config/environment.rb
+++ config/environment.rb
@@ -1,22 +1,15 @@
-#
 # Be sure to restart your web server when you modify this file.
-#
-# $Id$
-#
-# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
-# Revision:: $LastChangedRevision$
-# License:: GPL2
 
-
-# Uncomment below to force Rails into production mode when 
+# Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
+# Lucie: this line should stay comented out
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
+require File.join( File.dirname( __FILE__ ), 'boot' )
 
 Rails::Initializer.run do | config |
   # Settings in config/environments/* take precedence over those specified here
@@ -46,7 +39,7 @@ Rails::Initializer.run do | config |
     end
   end
 
-  
+
   # Skip frameworks you're not going to use (only works if using vendor/rails)
   config.frameworks -= [ :active_record, :action_web_service ]
 
@@ -57,40 +50,8 @@ Rails::Initializer.run do | config |
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
   config.load_paths << "#{RAILS_ROOT}/builder_plugins/installed"
 
-  # Force all environments to use the same logger level 
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
-
-  # Use the database for sessions instead of the file system
-  # (create the session table with 'rake db:sessions:create')
-  # config.action_controller.session_store = :active_record_store
-
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper, 
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector
-
-  # Make Active Record use UTC-base instead of local time
-  # config.active_record.default_timezone = :utc
-  
   # See Rails::Configuration for more options
 end
-
-# Add new inflection rules using the following format 
-# (all these examples are active by default):
-# Inflector.inflections do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
-
-# Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
-# Mime::Type.register "application/x-mobile", :mobile
 
 # Include your application configuration below
 
