5b920f04a2ce0cf9f58fde0bfda63c497fbbc152
Yasuhito Takamiya <yasuhito@gmail.com>
2007-10-02 08:01:51 +0000

    Subversion のスペックを追加 (ref #114)

--- app/models/subversion.rb
+++ app/models/subversion.rb
@@ -1,11 +1,3 @@
-#
-# $Id$
-#
-# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
-# Revision:: $LastChangedRevision$
-# License:: GPL2
-
-
 class Subversion
   include CommandLine
 
@@ -15,10 +7,15 @@ class Subversion
   attr_accessor :password
 
 
-  def initialize(options = {})
-    @url, @username, @password, @interactive = 
-          options.delete(:url), options.delete(:username), options.delete(:password), options.delete(:interactive)
-    raise "don't know how to handle '#{options.keys.first}'" if options.length > 0
+  def initialize options = {}
+    @url = options.delete( :url )
+    @username = options.delete( :username )
+    @password = options.delete( :password )
+    @interactive = options.delete( :interactive )
+
+    if options.length > 0
+      raise "don't know how to handle '#{ options.keys.first }'"
+    end
   end
 
 
@@ -26,9 +23,15 @@ class Subversion
     @url or raise 'URL not specified'
 
     options = "#{ @url } #{ target_directory }"
-    options << " --username #{ @username }" if username
-    options << " --password #{ @password }" if password
-    options << " --revision #{ revision_number( revision ) }" if revision
+    if username
+      options << " --username #{ @username }"
+    end
+    if password
+      options << " --password #{ @password }"
+    end
+    if revision
+      options << " --revision #{ revision_number( revision ) }"
+    end
 
     # need to read from command output, because otherwise tests break
     execute( svn( :co, options ) ) do | io |
@@ -37,33 +40,33 @@ class Subversion
   end
 
 
-  def latest_revision(installer)
+  def latest_revision installer
     last_locally_known_revision = info( installer ).last_changed_revision
-    svn_output = execute_in_local_copy( installer, svn( :log, "--revision HEAD:#{last_locally_known_revision} --verbose --xml" ) )
+    svn_output = execute_in_local_copy( installer, svn( :log, "--revision HEAD:#{ last_locally_known_revision } --verbose --xml" ) )
     SubversionLogParser.new.parse_log( svn_output ).first
   end
 
 
-  def revisions_since(installer, revision_number)
-    svn_output = execute_in_local_copy(installer, svn(:log, "--revision HEAD:#{revision_number} --verbose --xml"))
-    new_revisions = SubversionLogParser.new.parse_log(svn_output).reverse
-    new_revisions.delete_if { |r| r.number == revision_number }
+  def revisions_since installer, revision_number
+    svn_output = execute_in_local_copy( installer, svn( :log, "--revision HEAD:#{ revision_number } --verbose --xml" ) )
+    new_revisions = SubversionLogParser.new.parse_log( svn_output ).reverse
+    new_revisions.delete_if { | r | r.number == revision_number }
     new_revisions
   end
 
 
-  def update(installer, revision = nil)
-    revision_number = revision ? revision_number(revision) : 'HEAD'
-    svn_output = execute_in_local_copy(installer, svn(:update, "--revision #{revision_number}"))
-    SubversionLogParser.new.parse_update(svn_output)
+  def update installer, revision = nil
+    revision_number = revision ? revision_number( revision ) : 'HEAD'
+    svn_output = execute_in_local_copy( installer, svn( :update, "--revision #{ revision_number }" ) )
+    SubversionLogParser.new.parse_update svn_output
   end
 
 
   private
 
 
-  def revision_number(revision)
-    revision.respond_to?(:number) ? revision.number : revision.to_i
+  def revision_number revision
+    revision.respond_to?( :number ) ? revision.number : revision.to_i
   end
 
 
@@ -89,18 +92,18 @@ class Subversion
   def execute_in_local_copy installer, command
     Dir.chdir( installer.local_checkout ) do
       err_file_path = installer.path + '/svn.err'
-      FileUtils.rm_f(err_file_path)
-      FileUtils.touch(err_file_path)
-      execute(command, :stderr => err_file_path) do |io| 
-        result = io.readlines 
-        begin 
-          error_message = File.open(err_file_path){|f|f.read}.strip.split("\n")[1] || ""
+      FileUtils.rm_f err_file_path
+      FileUtils.touch err_file_path
+      execute( command, :stderr => err_file_path ) do | io |
+        result = io.readlines
+        begin
+          error_message = File.open( err_file_path ){ | f | f.read }.strip.split( "\n" )[ 1 ] || ""
         rescue
           error_message = ""
         ensure
-          FileUtils.rm_f(err_file_path)
+          FileUtils.rm_f err_file_path
         end
-        raise BuilderError.new(error_message, "svn_error") unless error_message.empty?
+        raise BuilderError.new( error_message, "svn_error" ) unless error_message.empty?
         return result
       end
     end
---
+++ spec/models/subversion_spec.rb
@@ -0,0 +1,119 @@
+require File.dirname( __FILE__ ) + '/../spec_helper'
+
+
+describe Subversion do
+  include FileSandbox
+
+
+  before( :each ) do
+    @subversion = Subversion.new( :url => 'http://www.my.com/', :username => 'USERNAME', :password => 'PASSWORD' )
+  end
+
+
+  it 'should get recent revisions' do
+    revisions = [ Revision.new( 1 ), revision_one = Revision.new( 2 ), revision_one = Revision.new( 3 ) ]
+
+    @subversion.expects( :execute_in_local_copy ).with( 'INSTALLER', 'svn --non-interactive log --revision HEAD:1 --verbose --xml' ).returns( 'SVN_OUTPUT' )
+    SubversionLogParser.any_instance.expects( :parse_log ).with( 'SVN_OUTPUT' ).returns( revisions )
+
+    new_revisions = @subversion.revisions_since( 'INSTALLER', 1 )
+    new_revisions.size.should == 2
+    new_revisions[ 0 ].number.should == 3
+    new_revisions[ 1 ].number.should == 2
+  end
+
+
+  it 'should update' do
+    @subversion.expects( :execute_in_local_copy ).with( 'INSTALLER', 'svn --non-interactive update --revision HEAD' ).returns( 'SVN_OUTPUT' )
+    SubversionLogParser.any_instance.expects( :parse_update ).with( 'SVN_OUTPUT' )
+
+    lambda do
+      @subversion.update 'INSTALLER'
+    end.should_not raise_error
+  end
+
+
+  it 'should checkout with revision specified' do
+    in_sandbox do | sandbox |
+      @subversion.expects( :execute ).with( "svn --non-interactive co http://www.my.com/ #{ sandbox.root } --username USERNAME --password PASSWORD --revision 1" ).yields( StringIO.new )
+
+      lambda do
+        @subversion.checkout sandbox.root, 1
+      end.should_not raise_error
+    end
+  end
+
+
+  it 'should get latest revision' do
+    FileUtils.expects( :touch ).with( './svn.err' ).times( 2 )
+    File.expects( :open ).with( './svn.err' ).raises( RuntimeError ).times( 2 )
+    @subversion.expects( :execute ).with( 'svn --non-interactive info --xml', { :stderr => './svn.err' } ).yields( StringIO.new( info_entry ) )
+    @subversion.expects( :execute ).with( 'svn --non-interactive log --revision HEAD:10 --verbose --xml', { :stderr => './svn.err' } ).yields( StringIO.new( log_entry ) )
+
+    revision = @subversion.latest_revision( dummy_project )
+
+    assert_equal 18, revision.number
+  end
+
+
+  it 'should raise if initialized with unknown option' do
+    lambda do
+      Subversion.new :unknown_option => true
+    end.should raise_error
+  end
+
+
+  DummyProject = Struct.new :local_checkout, :path
+
+
+  def dummy_project
+    DummyProject.new '.', '.'
+  end
+
+
+  def info_entry
+    <<-EOF
+<?xml version="1.0"?>
+<info>
+<entry kind="dir" path="." revision="10">
+<url>https://lucie.is.titech.ac.jp/svn/trunk</url>
+<repository>
+  <root>https://lucie.is.titech.ac.jp/svn</root>
+  <uuid>67f3d07d-cb8c-4aff-9cba-f81f45f607f3</uuid>
+</repository>
+<wc-info>
+  <schedule>normal</schedule>
+</wc-info>
+<commit revision="10">
+  <author>alexey</author>
+  <date>2007-10-02T01:24:20.295589Z</date>
+</commit>
+</entry>
+</info>
+EOF
+  end
+
+
+  def log_entry
+    <<-EOF
+<log>
+<logentry revision="18">
+  <author>alexey</author>
+  <date>2006-01-11T13:58:58.000007Z</date>
+  <msg>Goofin around with integration test for the builder</msg>
+</logentry>
+<logentry revision="17">
+  <author>alexey</author>
+  <date>2006-01-11T12:44:54.000007Z</date>
+  <msg>Moved builder from vendor, made bulder's integration tests talk to a subversion repository in the local file system</msg>
+</logentry>
+<logentry revision="15">
+  <author>stellsmi</author>
+  <date>2006-01-11T10:37:32.000007Z</date>
+  <msg>integration test does a checkout</msg>
+</logentry>
+</log>
+EOF
+#'
+  end
+end
