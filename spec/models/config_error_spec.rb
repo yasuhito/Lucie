15a59ccbe00b08bae483214e4c1d00d29b287ec4
Yasuhito Takamiya <yasuhito@gmail.com>
2007-10-03 02:33:11 +0000

    リファクタリング (refs #114)

--- spec/models/config_error_spec.rb
+++ spec/models/config_error_spec.rb
@@ -2,12 +2,7 @@ require File.dirname( __FILE__ ) + '/../spec_helper'
 
 
 describe ConfigError do
-  before(:each) do
-    @config_error = ConfigError.new
-  end
-
-
   it 'should be kind of StandardError' do
-    @config_error.should be_kind_of( StandardError )
+    ConfigError.new.should be_kind_of( StandardError )
   end
 end
