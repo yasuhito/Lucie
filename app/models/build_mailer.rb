fbe0f51b6fff4c537a49b4556d2b03a04652f52e
Yasuhito Takamiya <yasuhito@gmail.com>
2007-10-03 00:34:29 +0000

    BuildMailer のスペックを追加 (refs #114)

--- app/models/build_mailer.rb
+++ app/models/build_mailer.rb
@@ -1,15 +1,15 @@
 class BuildMailer < ActionMailer::Base
-
-  def build_report(build, recipients, from, subject, message, sent_at = Time.now)
+  def build_report build, recipients, from, subject, message, sent_at = Time.now
     @subject    = "[Lucie] #{subject}"
-    @body       = {:build => build, :message => message}
+    @body       = { :build => build, :message => message }
     @recipients = recipients
     @from       = from
     @sent_on    = sent_at
     @headers    = {}
   end
 
-  def test(recipients,  sent_at = Time.now)
+
+  def test recipients, sent_at = Time.now
     @subject    = 'Test CI E-mail'
     @body       = {}
     @recipients = recipients
---
+++ spec/models/build_mailer_spec.rb
@@ -0,0 +1,62 @@
+require File.dirname( __FILE__ ) + '/../spec_helper'
+
+
+# [TODO] following tests makes little sense as it doesn't invoke any production code directly. How to test mailer properly?
+describe BuildMailer do
+  include ActionMailer::Quoting
+
+
+  FIXTURES_PATH = File.dirname( __FILE__ ) + '/../fixtures'
+  CHARSET = 'utf-8'
+
+
+  before( :each ) do
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+
+    @expected = TMail::Mail.new
+    @expected.set_content_type 'text', 'plain', { 'charset' => CHARSET }
+    @expected.mime_version = '1.0'
+  end
+
+
+  it 'should create test' do
+    Time.stubs( :now ).returns( Time.at( 100000 ) )
+
+    @expected.subject = 'Test CI E-mail'
+    @expected.body = read_fixture( 'test' )
+    @expected.date = Time.now
+    @expected.to = 'TO'
+
+    BuildMailer.create_test( 'TO', @expected.date ).encoded.should == @expected.encoded
+  end
+
+
+  it 'should create build report' do
+    Time.stubs( :now ).returns( Time.at( 100000 ) )
+
+    @expected.subject = '[Lucie] SUBJECT'
+    @expected.body = read_fixture( 'build_report' )
+    @expected.date = Time.now
+    @expected.from = 'FROM'
+    @expected.to = 'TO'
+
+    build = Object.new
+    build.stubs( :changeset ).returns( 'CHANGESET' )
+    build.stubs( :output ).returns( 'OUTPUT' )
+    build.stubs( :installer_settings ).returns( 'INSTALLER_SETTINGS' )
+
+    BuildMailer.create_build_report( build, 'TO', 'FROM', 'SUBJECT', 'Message', @expected.date ).encoded.should == @expected.encoded
+  end
+
+
+  def read_fixture action
+    IO.readlines "#{ FIXTURES_PATH }/build_mailer/#{ action }"
+  end
+
+
+  def encode subject
+    quoted_printable subject, CHARSET
+  end
+end
