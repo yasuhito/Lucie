4ee4c58c326216d4000499d37b6ee50a191c470f
Yasuhito Takamiya <yasuhito@gmail.com>
2007-10-16 09:18:30 +0000

    sed のオプションを修正 (refs #120)

--- config/demo/puppet/manifests/definitions/basic.pp
+++ config/demo/puppet/manifests/definitions/basic.pp
@@ -6,7 +6,7 @@ define ensure_key_value( $file, $key, $value, $delimiter = " " ) {
   }
 
   # update it if it already exists...
-  exec { "sed -i '' 's/^$key$delimiter.*$/$key$delimiter$value/g' $file":
+  exec { "sed -i -e 's/^$key$delimiter.*$/$key$delimiter$value/g' $file":
     unless => "grep -xqe '$key[[:space:]]*$delimiter[[:space:]]*$value' -- $file",
     path => "/bin:/usr/bin"
   }
