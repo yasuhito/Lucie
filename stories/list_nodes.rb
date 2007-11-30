2175f1807d54d2a459b9117421c697cf476547ce
Yasuhito Takamiya <yasuhito@gmail.com>
2007-11-30 02:12:58 +0000

    node list コマンド受け入れテストの pending を解消 (refs #124)

--- stories/list_nodes.rb
+++ stories/list_nodes.rb
@@ -1,8 +1,7 @@
 Story "list nodes with 'node' command",
 %(As a cluster administrator
-  I want to get a nodes list using 'node' command
-  So that I can get a nodes list) do
-
+  I want to get a node list using 'node' command
+  So that I can get a node list) do
 
   Scenario 'node list success' do
     Given 'TEST_NODE is already added and is disabled' do
@@ -17,32 +16,11 @@ netmask_address:255.255.255.0
     end
 
     When 'I run', './node list' do | command |
-      @message = output_with( command ).split( /\s+/ )
+      @message = output_with( command )
     end
 
-    Then 'the output should look like', %(
-Nodes directory = '/cygdrive/d/01projects/lucie/nodes'
- TEST_NODE (installer = DISABLED)
-) do | message |
-      @message.should == message.strip.split( /\s+/ )
-    end
-  end
-end
-
-
-Story 'Trace node enable command',
-%(As a cluster administrator
-  I can trace a failed 'node enable' command
-  So that I can report a detailed backtrace to Lucie developers) do
-
-  Scenario 'run node enable with --trace option' do
-    When 'I run a command that fails with --trace option' do
-      pending
-      @error_message = output_with( './node list --trace' )
-    end
-
-    Then 'I get backtrace' do
-      @error_message.should match( /^\s+from/ )
+    Then 'the output should look like', /TEST_NODE \(installer = DISABLED\)/ do | regexp |
+      @message.should match( regexp )
     end
   end
 end
