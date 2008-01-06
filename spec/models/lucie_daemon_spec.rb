3ac8e16da2fca803148e03c9f0d8b37b0af2e583
Yasuhito Takamiya <yasuhito@gmail.com>
2008-01-06 23:01:36 +0000

    r366@t42p:  yasuhito | 2007-12-22 18:11:38 +0900  LucieDaemon のスペックを更新

--- spec/models/lucie_daemon_spec.rb
+++ spec/models/lucie_daemon_spec.rb
@@ -1,57 +1,31 @@
 require File.dirname( __FILE__ ) + '/../spec_helper'
 
 
-# As a Installer
-# I want to delegate build of installers to root privileged LucieDaemon process via druby protocol
-# So that only LucieDaemon should run in root privilege
-
-describe LucieDaemon, 'when calling sudo' do
-  before( :each ) do | each |
-    @lucie_daemon = LucieDaemon.new
-  end
-
-
-  it 'should run command with root privilege' do
-    # expects
-    @lucie_daemon.expects( :execute ).with( 'DUMMY_COMMAND' )
-
-    # when
-    @lucie_daemon.sudo( 'DUMMY_COMMAND' )
-
-    # then
-    verify_mocks
-  end
-
-
-  it 'should call block with root privilege' do
-    # expects
-    @dummy_object.expects( :hello )
-
-    # when
-    @lucie_daemon.sudo do
-      @dummy_object.hello
-    end
-
-    # then
-    verify_mocks
-  end
-end
-
-
-# As a lucie commandline script,
-# I want to start druby lucie server
-# So that Lucie scripts can delegate operations to Lucie daemon via druby
+# As a Lucie commandline script,
+# I want to start daemonized druby server (Lucie daemon),
+# so that I can always delegate jobs to Lucie daemon.
 
 describe LucieDaemon, 'when starting Lucie daemon' do
-  it 'should start with druby enabled' do
+  it 'should be daemonized and druby enabled' do
     @lucie_daemon = LucieDaemon.new
     LucieDaemon.stubs( :new ).returns( @lucie_daemon )
 
     # expects
+    Daemon::Controller.expects( :fork ).yields.times( 2 ).returns( false )
+    Process.expects( :setsid )
+    Daemon::PidFile.expects( :store )
+    Dir.expects( :chdir )
+    File.expects( :umask )
+    STDIN.expects( :reopen )
+    STDOUT.expects( :reopen )
+    STDERR.expects( :reopen )
+    Daemon::Controller.expects( :trap )
+
     DRb.expects( :start_service ).with( 'druby://localhost:58243', @lucie_daemon )
+    LucieDaemon.expects( :sleep )
 
     # when
-    LucieDaemon.start_service
+    LucieDaemon.daemonize
 
     # then
     verify_mocks
@@ -59,29 +33,34 @@ describe LucieDaemon, 'when starting Lucie daemon' do
 end
 
 
-# As a 'node install' command,
-# I want to make sure LucieDaemon can handle restart of Puppet daemon
-# so that I can delegate restart of Puppet daemon to LucieDaemon object
+# Stubbing LucieDaemon.daemonize
 
-describe LucieDaemon, 'when calling restart_puppet' do
-  it 'should restart Puppet daemon' do
-    # expects
-    Puppet.expects( :restart )
-
-    # when
-    LucieDaemon.new.restart_puppet
-
-    # then
-    verify_mocks
-  end
-end
-
-
-describe LucieDaemon, 'when calling sudo via druby' do
+describe 'Lucie Daemon (daemon disabled)', :shared => true do
   before( :each ) do
-    LucieDaemon.start_service
+    Daemon::Controller.stubs( :fork ).yields.returns( false )
+    Process.stubs( :setsid )
+    Daemon::PidFile.stubs( :store )
+    File.stubs( :umask )
+    STDIN.stubs( :reopen )
+    STDOUT.stubs( :reopen )
+    STDERR.stubs( :reopen )
+    Daemon::Controller.stubs( :trap )
+
+    LucieDaemon.stubs( :sleep )
+
+    LucieDaemon.daemonize
+
     @remote_lucie_daemon = DRbObject.new_with_uri( LucieDaemon.uri )
   end
+end
+
+
+# As a Lucie commandline script,
+# I want to delegate jobs to root privileged Lucie daemon via druby protocol,
+# so that I can be executed without root privilege.
+
+describe LucieDaemon, 'when calling sudo via druby' do
+  it_should_behave_like 'Lucie Daemon (daemon disabled)'
 
 
   after( :each ) do
@@ -92,45 +71,77 @@ describe LucieDaemon, 'when calling sudo via druby' do
   it 'should raise CommandLine::ExecutionError if invalid command is executed' do
     # when
     lambda do
-      @remote_lucie_daemon.sudo( 'hoge' )
+      @remote_lucie_daemon.sudo( 'INVALID_COMMAND' )
     end.should raise_error( CommandLine::ExecutionError )
 
     # then
     verify_mocks
-
   end
 
 
-  it 'should execute pwd command' do
+  it 'should execute pwd command without errors' do
     # when
     lambda do
-      @remote_lucie_daemon.sudo( 'pwd' )
+      @remote_lucie_daemon.sudo( 'pwd 2>&1 >/dev/null' )
+
+      # then
     end.should_not raise_error
   end
 
 
-  it 'should execute code block' do
+  it 'should execute code block without errors' do
     # expects
-    io = Object.new
-    io.expects( :puts ).with( 'hello' )
+    io_mock = Object.new
+    io_mock.expects( :puts ).with( 'HELLO LUCIE DAEMON' )
 
     # when
     lambda do
       @remote_lucie_daemon.sudo do
-        io.puts "hello"
+        io_mock.puts 'HELLO LUCIE DAEMON'
       end
+
+      # then
     end.should_not raise_error
+    
+    verify_mocks
+  end
+
+
+  it 'should raise if passed code block raises' do
+    # when
+    lambda do
+      @remote_lucie_daemon.sudo do
+        raise 'ERROR'
+      end
+
+      # then
+    end.should raise_error( RuntimeError, 'ERROR' )
   end
 end
 
 
 # As a 'node install' command,
-# I want to delegate restart of Puppet daemon to root privileged LucieDaemon process via druby protocol
-# so that I can restart Puppet daemon without root privileges
+# I want to delegate restart process of Puppet daemon to root privileged Lucie daemon via druby protocol,
+# so that I can restart Puppet daemon without root privilege.
 
 describe LucieDaemon, 'when calling restart_puppet via druby' do
+  it_should_behave_like 'Lucie Daemon (daemon disabled)'
+
+
   after( :each ) do
     DRb.stop_service
+  end
+
+
+  it 'should restart Puppet daemon if Lucie daemon is started' do
+    # expects
+    Puppet.expects( :restart )
+
+    # when
+    @remote_lucie_daemon.restart_puppet
+
+    # then
+    verify_mocks
   end
 
 
@@ -140,29 +151,9 @@ describe LucieDaemon, 'when calling restart_puppet via druby' do
 
     lambda do
       # when
-      remote_lucie_daemon = DRbObject.new_with_uri( LucieDaemon.uri )
-      remote_lucie_daemon.restart_puppet
+      @remote_lucie_daemon.restart_puppet
 
       # then
     end.should raise_error
-
-    # and
-    verify_mocks
-  end
-
-
-  it 'should restart Puppet daemon if Lucie daemon is started' do
-    # given
-    LucieDaemon.start_service
-
-    # expects
-    Puppet.expects( :restart )
-
-    # when
-    remote_lucie_daemon = DRbObject.new_with_uri( LucieDaemon.uri )
-    remote_lucie_daemon.restart_puppet
-
-    # then
-    verify_mocks
   end
 end
