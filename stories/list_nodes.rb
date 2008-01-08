ebf74a521f0d4577e61ea341eecb3345f8766ab6
Yasuhito Takamiya <yasuhito@gmail.com>
2008-01-08 02:43:47 +0000

    node list コマンドの受け入れテストを追加 (fixes #135)

--- stories/list_nodes.rb
+++ stories/list_nodes.rb
@@ -1,3 +1,6 @@
+require 'stories/helper'
+
+
 Story "list nodes with 'node' command",
 %(As a cluster administrator
   I want to get a node list using 'node' command
@@ -16,11 +19,11 @@ netmask_address:255.255.255.0
     end
 
     When 'I run', './node list' do | command |
-      @message = output_with( command )
+      @stdout, @stderr = output_with( command )
     end
 
     Then 'the output should look like', /TEST_NODE \(installer = DISABLED\)/ do | regexp |
-      @message.should match( regexp )
+      @stdout.should match( regexp )
     end
   end
 end
