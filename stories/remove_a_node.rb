504ba04c3b0a3f10dff6d6482df7a9f85e46c967
Yasuhito Takamiya <yasuhito@gmail.com>
2008-01-08 00:57:22 +0000

    node remove コマンドの受け入れテストを追加 (fixes #132) node remove コマンド実行時にビルダーを起動しないように修正

--- stories/remove_a_node.rb
+++ stories/remove_a_node.rb
@@ -1,9 +1,16 @@
+require 'stories/helper'
+
+
 Story "Remove a node with 'node remove' command", %(
   As a cluster administrator
   I want to remove a node using 'node remove' command
   So that I can remove a node from the system) do
 
   Scenario 'remove a node' do
+    Given 'No node is added' do
+      cleanup_nodes
+    end
+
     Given 'a node named', 'TEST_NODE' do | node_name |
       add_fresh_node node_name
     end
@@ -19,13 +26,11 @@ Story "Remove a node with 'node remove' command", %(
 
   Scenario 'node remove fails' do
     When 'I remove non-existing node named', 'NO_SUCH_NODE' do | node_name |
-      Open3.popen3( "./node remove #{ node_name } 2>&1 >/dev/null" ) do | stdin, stdout, stderr |
-        @error_message = stdout.read.chomp
-      end
+      @stdout, @stderr = output_with( "./node remove #{ node_name }" )
     end
 
     Then 'I get error look like:', "FAILED: Node 'NO_SUCH_NODE' not found." do | message |
-      @error_message.should == message
+      @stderr.should == message
     end
   end
 end
@@ -33,17 +38,24 @@ end
 
 Story 'Trace node remove command',
 %(As a cluster administrator
-  I can trace a failed command
+  I can trace a failed 'node remove' command
   So that I can report detailed backtrace to Lucie developers) do
 
   Scenario 'run node remove with --trace option' do
     When 'I run a command that fails with --trace option' do
-      @error_message = output_with( './node remove NO_SUCH_NODE --trace' )
+      @stdout, @stderr = output_with( './node remove NO_SUCH_NODE --trace' )
     end
 
     Then 'I get backtrace' do
-      @error_message.should match( /\(RuntimeError\)/ )
-      @error_message.should match( /^\s+from/ )
+      @stderr.should match( /\(RuntimeError\)/ )
+      @stderr.should match( /from/ )
     end
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
