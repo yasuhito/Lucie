130fb4cb16b0f4b043941c8eeb1a939dceba5f88
Yasuhito Takamiya <yasuhito@gmail.com>
2008-01-14 22:58:44 +0000

    r433@t42p:  yasuhito | 2008-01-14 15:03:45 +0900  added

---
+++ spec/models/wake_on_lan_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname( __FILE__ ) + '/../spec_helper'
+
+
+describe WakeOnLan, 'when calling WakeOnLan.wake' do
+  it 'should send magick packet' do
+    socket = Object.new
+
+    # expects
+    UDPSocket.expects( :open ).returns( socket )
+    socket.expects( :setsockopt ).with( Socket::SOL_SOCKET, Socket::SO_BROADCAST, 1 )
+    socket.expects( :send )
+    socket.expects( :close )
+
+    # when
+    WakeOnLan.wake '00:00:00:00:00:00'
+  end
+end
