78085b55404643a1496b642b80fe7945e1f70543
Yasuhito Takamiya <yasuhito@gmail.com>
2008-03-21 16:27:48 +0000

    r611@t42p:  yasuhito | 2008-03-22 01:27:21 +0900  ノードを複数台同時にインストールするコマンドを実験的に追加してみました。

---
+++ config/environments/install_multi.rb
@@ -0,0 +1,11 @@
+if File.exists?( "#{ RAILS_ROOT }/config/site_config.rb" )
+  require 'site_config'
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
+
