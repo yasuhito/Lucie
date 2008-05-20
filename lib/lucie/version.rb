494e25fb4a7bc1cc34ec2ef9b6f7105535e97bf8
Yasuhito Takamiya <yasuhito@gmail.com>
2008-05-20 05:06:02 +0000

    version 0.3.1

--- lib/lucie/version.rb
+++ lib/lucie/version.rb
@@ -3,7 +3,7 @@ module Lucie
     unless defined? MAJOR
       MAJOR = 0
       MINOR = 3
-      MAINTENANCE = 0
+      MAINTENANCE = 1
 
       STRING = [ MAJOR, MINOR, MAINTENANCE ].join( '.' )
     end
