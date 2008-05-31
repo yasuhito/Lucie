facc4a74f21c6b25ddda59981d04cefddc3982f1
Yasuhito Takamiya <yasuhito@gmail.com>
2008-05-31 15:20:40 +0000

    r704@t42p:  yasuhito | 2008-06-01 00:19:51 +0900  ひとまず dhcp.rb をリファクタリング (refs #174)

--- app/models/dhcp.rb
+++ app/models/dhcp.rb
@@ -1,3 +1,12 @@
+#
+# dhcp.rb - setups dhcp server
+#
+# methods:
+#
+#   Dhcp.setup - setups dhcp server
+#
+
+
 require 'facter'
 require 'ftools'
 require 'popen3/shell'
@@ -5,45 +14,12 @@ require 'resolv'
 
 
 class Dhcp
+  #
+  # automagically generates /etc/dhcp3/dhcpd.conf and restarts dhcp
+  # server
+  #
   def self.setup
-    self.new.setup
-  end
-
-
-  def setup
-    if subnet == {}
-      return
-    end
-
-    check_dhcpd_installed
-
-    File.copy config_file, config_file + '.orig'
-
-    File.open( config_file, 'w' ) do | file |
-      subnet.each_key do | each |
-        first = subnet[ each ].first
-        file.puts <<-EOF
-option domain-name "#{ domain }";
-
-subnet #{ each[ 0 ] } netmask #{ each[ 1 ] } {
-  option routers #{ first.gateway_address };
-  option broadcast-address #{ Network.broadcast_address( first.ip_address, first.netmask_address ) };
-  deny unknown-clients;
-
-  next-server #{ ipaddress };
-  filename "pxelinux.0";
-
-#{ host_entries( subnet[ each ] ) }
-}
-EOF
-      end
-    end
-
-    begin
-      sh_exec '/etc/init.d/dhcp3-server restart'
-    rescue
-      raise 'dhcpd server failed to start - check syslog for diagnostics.'
-    end
+    self.new.__send__ :setup
   end
 
 
@@ -52,14 +28,69 @@ EOF
   ################################################################################
 
 
-  def check_dhcpd_installed
-    unless File.exists?( '/usr/sbin/dhcpd3' )
+  def setup
+    if all_subnets == {}
+      # [???] emit warning?
+      return
+    end
+
+    unless dhcpd_installed
       raise 'dhcp3-server package is not installed. Please install first.'
+    end
+
+    generate_config_file
+
+    begin
+      sh_exec '/etc/init.d/dhcp3-server restart'
+    rescue
+      # [???] revert to original dhcpd.conf?
+      raise 'dhcpd server failed to start - check syslog for diagnostics.'
     end
   end
 
 
-  def subnet
+  def generate_config_file
+    File.copy config_file, config_file + '.orig'
+    File.open( config_file, 'w' ) do | file |
+      all_subnets.each_pair do | netinfo, nodes_in_subnet |
+        subnet = netinfo[ 0 ]
+        netmask = netinfo[ 1 ]
+
+        first_node = nodes_in_subnet.first
+        router = first_node.gateway_address
+        broadcast = Network.broadcast_address( first_node.ip_address, first_node.netmask_address )
+
+        file.puts <<-EOF
+option domain-name "#{ domain }";
+
+subnet #{ subnet } netmask #{ netmask } {
+  option routers #{ router };
+  option broadcast-address #{ broadcast };
+  deny unknown-clients;
+
+  next-server #{ ipaddress };
+  filename "pxelinux.0";
+
+#{ host_entries( nodes_in_subnet ) }
+}
+EOF
+      end
+    end
+  end
+
+
+  def dhcpd_installed
+    File.exists?( '/usr/sbin/dhcpd3' )
+  end
+
+
+  #
+  # returns all the subnet and netmask addresses used by enabled nodes.
+  #
+  # return value:
+  #   a hash of [ network_address, netmask_address ] => [ node1, node2, ... ]
+  #
+  def all_subnets
     sn = Hash.new( [] )
 
     Nodes.load_all.select do | each |
