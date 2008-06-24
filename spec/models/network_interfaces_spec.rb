100ac8d8780a407ddf70614717b8322dbb29ae99
Yasuhito Takamiya <yasuhito@gmail.com>
2008-06-24 02:26:52 +0000

    network_interfaces.rb のカバレッジを 100% に改善 (refs #47)

--- spec/models/network_interfaces_spec.rb
+++ spec/models/network_interfaces_spec.rb
@@ -4,9 +4,9 @@ require File.dirname( __FILE__ ) + '/../spec_helper'
 describe NetworkInterfaces, 'when accessing network information using each iterator' do
   it 'should have interfaces for network info' do
     NetworkInterfaces.each do | each |
-      each.should respond_to( :netmask )
-      each.should respond_to( :ipaddress )
-      each.should respond_to( :subnet )
+      each.netmask.should_not be_nil
+      each.ipaddress.should_not be_nil
+      each.subnet.should_not be_nil
     end
   end
 end
