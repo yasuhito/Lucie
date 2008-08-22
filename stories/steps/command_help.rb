b8981d9b786aef80135c655900a77fe97a732673
Yasuhito Takamiya <yasuhito@gmail.com>
2008-08-22 15:37:06 +0000

    node list のヘルプメッセージ用 RSpec ストーリーをリファクタリング (refs #114)

--- stories/steps/command_help.rb
+++ stories/steps/command_help.rb
@@ -1,15 +1,15 @@
 steps_for( :command_help ) do
-  When( 'I run $command' ) do | command |
+  When "I run '$command'" do | command |
     @help_message, = output_with( command )
   end
 
 
-  Then( 'the first line of the help message should be a usage example' ) do
-    @help_message.split( "\n" ).first.should match( /^usage:/ )
+  Then 'the first line of help message should be a usage example' do
+    @help_message.split( "\n" ).first.should match( /^Usage:/ )
   end
 
 
-  Then( 'the help message should include a description of $short_option and $long_option option' ) do | sopt, lopt |
+  Then "the help message should include a description of '$short_option' and '$long_option' option" do | sopt, lopt |
     extract_options( @help_message ).include?( [ sopt, lopt ] ).should be_true
   end
 end
