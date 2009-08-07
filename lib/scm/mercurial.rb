fd8daa75f3427f83cbbd6db8a34ae00529870839
Yasuhito Takamiya <yasuhito@gmail.com>
2009-08-07 07:48:33 +0000

    fixed

--- lib/scm/mercurial.rb
+++ lib/scm/mercurial.rb
@@ -6,19 +6,19 @@ module Scm
 
 
     def update_commands_for target
-      [ "cd #{ target } && hg pull --ssh 'ssh -l #{ whoami } #{ SSH::OPTIONS }'",
-        "cd #{ target } && hg update --ssh 'ssh -l #{ whoami } #{ SSH::OPTIONS }'" ]
+      [ "cd #{ target } && hg pull --ssh 'ssh -l #{ whoami } -i #{ SSH::PRIVATE_KEY } #{ SSH::OPTIONS }'",
+        "cd #{ target } && hg update --ssh 'ssh -l #{ whoami } -i #{ SSH::PRIVATE_KEY } #{ SSH::OPTIONS }'" ]
     end
 
 
     def update target
-      run "cd #{ target } && hg pull --ssh 'ssh -l #{ whoami } #{ SSH::OPTIONS }'"
-      run "cd #{ target } && hg update --ssh 'ssh -l #{ whoami } #{ SSH::OPTIONS }'"
+      run "cd #{ target } && hg pull --ssh 'ssh -l #{ whoami } -i #{ SSH::PRIVATE_KEY } #{ SSH::OPTIONS }'"
+      run "cd #{ target } && hg update --ssh 'ssh -l #{ whoami } -i #{ SSH::PRIVATE_KEY } #{ SSH::OPTIONS }'"
     end
 
 
     def install_command target, server_ip, url
-      "scp #{ SSH::OPTIONS } -r #{ server_ip }:#{ Configurator::Server.clone_clone_directory( url ) } #{ target }"
+      "scp -i #{ SSH::PRIVATE_KEY } #{ SSH::OPTIONS } -r #{ server_ip }:#{ Configurator::Server.clone_clone_directory( url ) } #{ target }"
     end
 
 
