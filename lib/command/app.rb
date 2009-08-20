4a10d4b68852cb2d1624f0aade85b64e43606687
Yasuhito Takamiya <yasuhito@gmail.com>
2009-08-20 00:11:50 +0000

    refactoring

--- lib/command/app.rb
+++ lib/command/app.rb
@@ -147,7 +147,7 @@ module Command
 
     def setup_ldb
       return unless @options.ldb_repository
-      @configurator = Configurator.new( debug_options )
+      @configurator = Configurator.new( "Mercurial", debug_options )
       if FileTest.directory?( Configurator::Server.clone_directory( @options.ldb_repository ) )
         @configurator.update_server @options.ldb_repository
       else
