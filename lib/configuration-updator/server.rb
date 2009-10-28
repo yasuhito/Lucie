e9d187a462a05d7914272d8ff812439262943aee
Yasuhito Takamiya <yasuhito@gmail.com>
2009-10-28 14:15:44 +0000

    refactoring

--- lib/configuration-updator/server.rb
+++ lib/configuration-updator/server.rb
@@ -1,40 +1,30 @@
-require "configuration"
 require "scm"
 
 
 class ConfigurationUpdator
   class Server
     def initialize debug_options = {}
-      @scm = Scm.new( debug_options )
+      @debug_options = debug_options
     end
 
 
     def update repos_name
       begin
-        test_local_clone_exists? repos_name
-        scm = @scm.from( local_clone_directory( repos_name ) )
+        target = local_clone_directory( repos_name )
+        unless FileTest.directory?( target )
+          raise "Configuration repository #{ target } not found on Lucie server."
+        end
+        scm = Scm.new( @debug_options ).from( target )
         scm.test_installed
-        scm.update local_clone_directory( repos_name )
+        scm.update target
       rescue => e
-        raise "Failed to update #{ local_clone_directory repos_name }: #{ e.message }"
+        raise "Failed to update #{ target }: #{ e.message }"
       end
     end
 
 
     def local_clone_directory repos_name
       File.join Configurator::Server.config_directory, repos_name
-    end
-
-
-    ############################################################################
-    private
-    ############################################################################
-
-
-    def test_local_clone_exists? repos_name
-      unless FileTest.directory?( local_clone_directory( repos_name ) )
-        raise "Configuration repository #{ local_clone_directory repos_name } not found on Lucie server."
-      end
     end
   end
 end
