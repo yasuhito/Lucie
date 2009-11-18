e06b43239a7074e0194632527edda7fddb59d8b9
Yasuhito Takamiya <yasuhito@gmail.com>
2009-11-18 07:42:36 +0000

    refactoring

--- lib/ssh-home.rb
+++ lib/ssh-home.rb
@@ -2,9 +2,15 @@ require "lucie/utils"
 
 
 module SSHHome
+  include Lucie::Debug
+  include Lucie::Utils
+
+
   def setup_ssh_home target
-    Lucie::Utils.mkdir_p target, @debug_options unless FileTest.directory?( target )
-    Lucie::Utils.run "chmod 0700 #{ target }", @debug_options
+    mkdir_p target, @debug_options unless FileTest.directory?( target )
+    if dry_run || File.stat( target ).mode.to_s( 8 ) != "40700"
+      run "chmod 0700 #{ target }", @debug_options
+    end
   end
 end
 
