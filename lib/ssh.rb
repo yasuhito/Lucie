b25bc549bdbdbb2c5a76d950731fc3139e4f9a93
Yasuhito Takamiya <yasuhito@gmail.com>
2009-11-19 00:31:38 +0000

    refactoring: extract a code block of SSH class as SSH::Cp class

--- lib/ssh.rb
+++ lib/ssh.rb
@@ -2,6 +2,7 @@ require "lucie"
 require "lucie/debug"
 require "lucie/logger/null"
 require "lucie/utils"
+require "ssh/cp"
 require "ssh/key-pair-generator"
 require "ssh/nfsroot"
 require "ssh/sh"
@@ -57,17 +58,7 @@ class SSH
 
   def cp ip, from, to
     SubProcess::Shell.open( @debug_options ) do | shell |
-      command = "scp -i #{ private_key_path } #{ OPTIONS } #{ from } root@#{ ip }:#{ to }"
-      shell.on_stdout do | line |
-        stdout.puts line
-      end
-      shell.on_stderr do | line |
-        stderr.puts line
-      end
-      shell.on_failure do
-        raise "command #{ command } failed on #{ ip }"
-      end
-      shell.exec command
+      Cp.new( from, "#{ ip }:#{ to }", private_key_path, @debug_options ).run( shell )
     end
   end
 
---
+++ lib/ssh/cp.rb
@@ -0,0 +1,32 @@
+class SSH
+  class Cp
+    def initialize from, to, private_key_path, debug_options
+      @from = from
+      @to = to
+      @private_key_path = private_key_path
+      @debug_options = debug_options
+    end
+
+
+    def run shell
+      command = "scp -i #{ @private_key_path } #{ SSH::OPTIONS } #{ @from } root@#{ @to }"
+      shell.on_stdout do | line |
+        stdout.puts line
+      end
+      shell.on_stderr do | line |
+        stderr.puts line
+      end
+      shell.on_failure do
+        raise "command #{ command } failed"
+      end
+      shell.exec command
+    end
+  end
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
