cfc221bcc6ab72297a64234b38aefc28cd30dd1f
Yasuhito Takamiya <yasuhito@gmail.com>
2009-11-27 01:34:22 +0000

    refactoring

--- lib/lucie.rb
+++ lib/lucie.rb
@@ -1,17 +1,13 @@
-#
-# NOTE: Please require this file first before other lib/**/*.rb are required.
-#
-# e.g.
-#   require 'lucie'
-#   require 'foo'
-#   require 'bar'
-#      ...
-#
-
-
 $LOAD_PATH.unshift( File.expand_path( File.dirname( __FILE__ ) + "/../vendor/ruby-ifconfig-1.2/lib/" ) )
 
 
 module Lucie
   ROOT = File.expand_path( File.join( File.dirname( __FILE__ ), ".." ) )
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
---
+++ lib/lucie/script.rb
@@ -0,0 +1,20 @@
+module Lucie
+  module Script
+    def handle_exception app, e
+      $stderr.puts "ERROR: " + e.message
+      if app and app.options.verbose
+        e.backtrace.each do | each |
+          $stderr.puts each
+        end
+      end
+      exit -1
+    end
+  end
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
