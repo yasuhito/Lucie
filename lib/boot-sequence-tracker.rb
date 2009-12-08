f090cc62fb997bd8a6e8029c4457fa39147ef9c3
Yasuhito Takamiya <yasuhito@gmail.com>
2009-12-08 08:32:55 +0000

    refactoring

--- lib/boot-sequence-tracker.rb
+++ lib/boot-sequence-tracker.rb
@@ -14,10 +14,11 @@ class BootSequenceTracker
 
 
   def initialize node, logger, debug_options = {}
-    @node = node
-    @dhcpd_re = DhcpdRE.new( @node )
-    @nfsd_re = NfsdRE.new( @node )
-    @tftpd_re = TftpdRE.new( @node )
+    @node_name = node.name
+    @node_ip = node.ip_address
+    @dhcpd_re = DhcpdRE.new( node )
+    @nfsd_re = NfsdRE.new( node )
+    @tftpd_re = TftpdRE.new( node )
     @logger = logger
     @debug_options = debug_options
   end
@@ -30,62 +31,62 @@ class BootSequenceTracker
 
 
   def wait_pxe
-    block_until_match @tftpd_re.pxelinux, "Waiting for #{ name } to request PXE boot loader ..."
-    block_until_match @tftpd_re.pxelinux_cfg, "Waiting for #{ name } to request PXE boot loader configuration file ..."
-    block_until_match @tftpd_re.kernel, "Waiting for #{ name } to request Lucie kernel ..."
+    block_until_match @tftpd_re.pxelinux, "Waiting for #{ @node_name } to request PXE boot loader ..."
+    block_until_match @tftpd_re.pxelinux_cfg, "Waiting for #{ @node_name } to request PXE boot loader configuration file ..."
+    block_until_match @tftpd_re.kernel, "Waiting for #{ @node_name } to request Lucie kernel ..."
   end
 
 
   def wait_manual_reboot
-    block_until_match @tftpd_re.pxelinux, "Please reboot #{ name } manually."
-    block_until_match @tftpd_re.pxelinux_cfg, "Waiting for #{ name } to request PXE boot loader configuration file ..."
-    block_until_match @tftpd_re.kernel, "Waiting for #{ name } to request Lucie kernel ..."
+    block_until_match @tftpd_re.pxelinux, "Please reboot #{ @node_name } manually."
+    block_until_match @tftpd_re.pxelinux_cfg, "Waiting for #{ @node_name } to request PXE boot loader configuration file ..."
+    block_until_match @tftpd_re.kernel, "Waiting for #{ @node_name } to request Lucie kernel ..."
   end
 
 
   def wait_pxe_localboot
-    block_until_match @tftpd_re.pxelinux, "Waiting for #{ name } to request PXE boot loader ..."
-    block_until_match @tftpd_re.pxelinux_cfg, "Waiting for #{ name } to request PXE boot loader configuration file ..."
+    block_until_match @tftpd_re.pxelinux, "Waiting for #{ @node_name } to request PXE boot loader ..."
+    block_until_match @tftpd_re.pxelinux_cfg, "Waiting for #{ @node_name } to request PXE boot loader configuration file ..."
   end
 
 
   def wait_dhcpack
-    block_until_match @dhcpd_re.discover, "Waiting for #{ name } to send DHCPDISCOVER ..."
-    block_until_match @dhcpd_re.offer, "Waiting for #{ name } to receive DHCPOFFER ..."
-    block_until_match @dhcpd_re.request, "Waiting for #{ name } to send DHCPREQUEST ..."
-    block_until_match @dhcpd_re.ack, "Waiting for #{ name } to receive DHCPACK ..."
+    block_until_match @dhcpd_re.discover, "Waiting for #{ @node_name } to send DHCPDISCOVER ..."
+    block_until_match @dhcpd_re.offer, "Waiting for #{ @node_name } to receive DHCPOFFER ..."
+    block_until_match @dhcpd_re.request, "Waiting for #{ @node_name } to send DHCPREQUEST ..."
+    block_until_match @dhcpd_re.ack, "Waiting for #{ @node_name } to receive DHCPACK ..."
   end
 
 
   def wait_nfsroot
-    block_until_match @nfsd_re.mount, "Waiting for #{ name } to mount nfsroot ..."
+    block_until_match @nfsd_re.mount, "Waiting for #{ @node_name } to mount nfsroot ..."
   end
 
 
   def wait_pong
     wait_loop do
-      debug "Waiting for #{ name } to respond to ping ..."
-      dry_run || ping
+      debug "Waiting for #{ @node_name } to respond to ping ..."
+      break if dry_run || ping
     end
   end
 
 
   def wait_no_pong
     wait_loop do
-      debug "Waiting for #{ name } to stop responding to ping ..."
-      dry_run || ( not ping )
+      debug "Waiting for #{ @node_name } to stop responding to ping ..."
+      break if dry_run || ( not ping )
     end
   end
 
 
   def wait_sshd
     wait_loop do
-      debug "Waiting for sshd to start on #{ name } ..."
+      debug "Waiting for sshd to start on #{ @node_name } ..."
       begin
-        TCPSocket.open( ip, 22 ) unless dry_run
-        true
+        TCPSocket.open( @node_ip, 22 ) unless dry_run
+        break
       rescue Errno::EHOSTUNREACH, Errno::ECONNREFUSED
-        false
+        # do nothing
       end
     end
   end
@@ -98,14 +99,14 @@ class BootSequenceTracker
 
   def wait_loop
     loop do
-      break if yield
+      yield
       sleep retry_interval
     end
   end
 
 
   def ping
-    Ping.pingecho ip
+    Ping.pingecho @node_ip
   end
 
 
@@ -113,14 +114,14 @@ class BootSequenceTracker
     raise "syslog is not set" unless @syslog
     wait_loop do
       debug message
-      tail regexp
+      break if tail( regexp )
     end
   end
 
 
   def tail regexp
-    while line = @syslog.gets
-      return true if regexp.match( line )
+    while @syslog.gets
+      return true if regexp.match( $_ )
     end
     false
   end
@@ -128,16 +129,6 @@ class BootSequenceTracker
 
   def retry_interval
     @debug_options[ :retry_interval ] || DEFAULT_RETRY_INTERVAL
-  end
-
-
-  def name
-    @node.name
-  end
-
-
-  def ip
-    @node.ip_address
   end
 end
 
--- spec/boot-sequence-tracker_spec.rb
+++ spec/boot-sequence-tracker_spec.rb
@@ -4,15 +4,18 @@ require File.join( File.dirname( __FILE__ ), "spec_helper" )
 describe BootSequenceTracker do
   before :each do
     node = mock( "node", :name => "yutaro", :ip_address => "192.168.0.1", :mac_address => "11:22:33:44:55:66" )
-    logger = mock( "logger", :debug => nil )
-    @tracker = BootSequenceTracker.new( node, logger, { :verbose => false, :retry_interval => 0.001 } )
+    logger = mock( "logger" ).as_null_object
+    @messenger = StringIO.new
+    @tracker = BootSequenceTracker.new( node, logger, { :verbose => true, :messenger => @messenger, :retry_interval => 0.001 } )
   end
 
 
   it "should wait until node boots from PXE" do
-    syslog = mock( "syslog", :seek => nil )
-    syslog.should_receive( :gets ).and_return( *dummy_pxe_syslog )
-    @tracker.syslog = syslog
+    @messenger.should_receive( :puts ).with( "Waiting for yutaro to request PXE boot loader ..." ).once.ordered
+    @messenger.should_receive( :puts ).with( "Waiting for yutaro to request PXE boot loader configuration file ..." ).once.ordered
+    @messenger.should_receive( :puts ).with( "Waiting for yutaro to request Lucie kernel ..." ).once.ordered
+
+    @tracker.syslog = dummy_syslog
     @tracker.wait_pxe
   end
 
@@ -31,13 +34,13 @@ describe BootSequenceTracker do
   end
 
 
-  def dummy_pxe_syslog
-    [ 
-     "Jun 17 21:00:15 lucie_server in.tftpd[12345]: RRQ from 192.168.0.1 filename pxelinux.0",
-     nil,
-     "Jun 17 21:00:16 lucie_server in.tftpd[12345]: RRQ from 192.168.0.1 filename pxelinux.cfg/01-11-22-33-44-55-66",
-     nil,
-     "Jun 17 21:00:17 lucie_server in.tftpd[12345]: RRQ from 192.168.0.1 filename lucie"
-    ]
+  def dummy_syslog
+    io = StringIO.new( <<-EOL )
+Jun 17 21:00:15 lucie_server in.tftpd[12345]: RRQ from 192.168.0.1 filename pxelinux.0
+Jun 17 21:00:16 lucie_server in.tftpd[12345]: RRQ from 192.168.0.1 filename pxelinux.cfg/01-11-22-33-44-55-66
+Jun 17 21:00:17 lucie_server in.tftpd[12345]: RRQ from 192.168.0.1 filename lucie
+EOL
+    io.stub! :seek
+    io
   end
 end
