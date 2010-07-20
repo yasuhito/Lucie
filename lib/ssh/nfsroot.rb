5cc4d476bd158c8ad61424bfe451e51cca21e347
Yasuhito Takamiya <yasuhito@gmail.com>
2010-07-20 13:04:08 +0000

    SSH::Nfsroot の RDoc を追加

--- lib/ssh/nfsroot.rb
+++ lib/ssh/nfsroot.rb
@@ -1,18 +1,35 @@
-require "lucie/debug"
 require "lucie/utils"
 require "ssh/home"
 require "ssh/path"
 
 
-#
-# Setups ssh access to nfsroot.
-#
-class SSH::Nfsroot # :nodoc:
-  include Lucie::Debug
+class SSH::Nfsroot
   include Lucie::Utils
   include SSH::Path
 
 
+  #
+  # Creates a new nfsroot configurator which sets up password-less SSH
+  # access to nfsroot. The following options are available:
+  #
+  # <tt>:logger</tt>:: Save logs with the specified logger [nil]
+  # <tt>:verbose</tt>:: Be verbose [nil] 
+  # <tt>:dry_run</tt>:: Print the commands that would be executed, but do not execute them. [nil]
+  #
+  # Usage:
+  #
+  #   # New nfsroot configurator
+  #   configurator = SSH::Nfsroot.new( "/tmp/nfsroot" )
+  #
+  #   # New nfsroot configurator, with logging
+  #   configurator = SSH::Nfsroot.new( "/tmp/nfsroot", :logger => logger )
+  #
+  #   # New nfsroot configurator, verbose mode
+  #   configurator = SSH::Nfsroot.new( "/tmp/nfsroot", :verbose => true )
+  #
+  #   # New nfsroot configurator, dry-run mode
+  #   configurator = SSH::Nfsroot.new( "/tmp/nfsroot", :dry_run => true )
+  #
   def initialize base_dir, debug_options
     @base_dir = base_dir
     @ssh_home = SSH::Home.new( nfsroot_ssh_home, debug_options )
@@ -20,10 +37,13 @@ class SSH::Nfsroot # :nodoc:
   end
 
 
+  #
+  # Sets up sshd and ssh homedir on nfsroot.
+  #
   def setup_ssh_access
     setup_sshd
     setup_ssh_home
-    info "ssh access to nfsroot configured.\n"
+    info "ssh access to nfsroot configured."
   end
 
 
